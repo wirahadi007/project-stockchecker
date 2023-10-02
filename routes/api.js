168a25780fbf8c3b195bd665579fc3e92543b9a7
Wirahadi Putra <charlesotojr@gmail.com>
2023-10-02 08:15:55 +0000

    fix(error): fixing error program

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,84 @@
 'use strict';
 
-module.exports = function (app) {
+var mongoose = require('mongoose')
+var objectId = mongoose.Types.ObjectId
+var request = require('request-promise-native')
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
+var stockSchema = new mongoose.Schema({
+  code: String,
+  likes: { type: [String], default: [] }
+})
+
+var Stock = mongoose.model('stock', stockSchema)
+
+function saveStock(code, like, ip) {
+  return Stock.findOne({ code: code })
+    .then(stock => {
+      if (!stock) {
+        let newStock = new Stock({ code: code, likes: like ? [ip] : [] })
+        return newStock.save()
+      } else {
+        if (like && stock.likes.indexOf(ip) === -1) {
+          stock.likes.push(ip)
+        }
+        return stock.save()
+      }
+    })
+}
+
+function parseData(data) {
+  let i = 0
+  let stockData = []
+  let likes = []
+  while (i < data.length) {
+    let stock = { stock: data[i].code, price: parseFloat(data[i+1]) }
+    likes.push(data[i].likes.length)
+    stockData.push(stock)
+    i += 2
+  }
+
+  if (likes.length > 1) {
+    stockData[0].rel_likes = likes[0] - likes[1]
+    stockData[1].rel_likes = likes[1] - likes[0]
+  } else {
+    stockData[0].likes = likes[0]
+    stockData = stockData[0]
+  }
+  
+  return stockData
+}
+
+module.exports = function (app) {
+  
+  app.get('/api/testing', (req, res) => {
+    console.log(req.connection)
     
-};
+    res.json({ IP: req.ip })
+  })
+  
+  app.route('/api/stock-prices')
+    .get(function (req, res) {
+      let code = req.query.stock || ''
+      if (!Array.isArray(code)) {
+        code = [code]
+      }
+    
+      let promises = []
+      code.forEach(code => {
+        promises.push(saveStock(code.toUpperCase(), req.query.like, req.ip))
+        
+        let url = `https://api.iextrading.com/1.0/stock/${code.toUpperCase()}/price`
+        promises.push(request(url))
+      })
+    
+      Promise.all(promises)
+        .then(data => {
+          let stockData = parseData(data)
+          res.json({ stockData })
+        })
+        .catch(err => {
+          console.log(err)
+          res.send(err)
+        })
+    })
+}
--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var expect      = require('chai').expect;
+var cors        = require('cors');
+var mongoose    = require('mongoose')
+var helmet      = require('helmet')
 
-const app = express();
+var apiRoutes         = require('./routes/api.js');
+var fccTestingRoutes  = require('./routes/fcctesting.js');
+var runner            = require('./test-runner');
+
+mongoose.Promise = global.Promise
+const dbName = 'stock-price-database'
+mongoose.connect(`${process.env.DB}${dbName}`, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', err => { console.error(err) })
+db.once('open', () => {
+  console.log('Connected to ' + dbName)
+})
+
+// Close MongoDB connection
+process.on('SIGINT', () => {
+  db.close(() => {
+    console.log(`Closing connection to ${dbName}`)
+    process.exit(0)
+  })
+})
+
+var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -16,6 +36,8 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet())
 
 //Index page (static HTML)
 app.route('/')
@@ -37,16 +59,17 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
       } catch(e) {
-        console.log('Tests are not valid:');
-        console.error(e);
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
       }
     }, 3500);
   }
